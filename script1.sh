4cc5029f7c9893ea0c7fd2e6dd9d1b1b6831990c
SSivaneswaran <144017797+SSivaneswaran@users.noreply.github.com>
2023-12-04 02:12:19 +0530

    Jenkins-Email

---
+++ script1.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo "This is shivas simple script and ran through Jenkins with Git "
+
+
+recipient="ssivaneswaran@gmail.com"
+subject="Shiva-Jenkins!"
+body="Hello, World! Email sent from Shive-Jenkins 'mutt'."
+
+#echo "$body" | mutt -s "$subject" -- $recipient
+
+
+
+
+
+~
+
+
