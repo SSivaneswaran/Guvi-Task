5995fe8b88e43327d7bd71e9330ffee24f5d3527
SSivaneswaran <144017797+SSivaneswaran@users.noreply.github.com>
2023-12-04 01:49:05 +0530

    Updatescript.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
 echo "This is shivas simple script and ran through Jenkins with Git "
+emailext body: 'Email sent from Shive-Jenkins', subject: 'Shive-Jenkins', to: 'ssivaneswaran@gmail.com'
