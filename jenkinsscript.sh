22673e34fb619d69a41a1f8843a70e78f2c0da25
root <root@ip-172-31-18-150.ec2.internal>
2023-12-24 02:27:29 +0000

    Jenkins-Task

---
+++ jenkinsscript.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+#
+##
+uname -a
+uptime -p
+#
+#
+touch {1..10}.txt
+#
+#
+ls
+#
+#
+echo "congratulations" > 1.txt
+echo "congratulations" > 2.txt
+echo "congratulations" > 3.txt
+echo "congratulations" > 4.txt
+echo "congratulations" > 5.txt
+echo "congratulations" > 6.txt
+echo "congratulations" > 7.txt
+echo "congratulations" > 8.txt
+echo "congratulations" > 9.txt
+echo "congratulations" > 10.txt
+#
+#
+echo $GVC
+
+
